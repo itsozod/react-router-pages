53631de941a654100cfdb08c07696d6507c8529e
Ozod <o.bukhorizoda@mail.ru>
2024-01-03 18:02:18 +0500

    added a loader

--- src/pages/users/Users.jsx
+++ src/pages/users/Users.jsx
@@ -5,9 +5,11 @@ import {
   getUsersListData,
   setFavourites,
 } from "../../store/features/usersList/usersListSlice";
+import { Loader } from "../../components/loader/Loader";
 
 export const Users = () => {
   const usersList = useSelector((state) => state.usersList.usersList);
+  const loader = useSelector((state) => state.usersList.loader);
   console.log(usersList);
   const dispatch = useDispatch();
 
@@ -16,21 +18,27 @@ export const Users = () => {
   }, [dispatch]);
 
   return (
-    <div className={styles.users_container}>
-      {usersList.map((user) => (
-        <article key={user.id} className={styles.user_card}>
-          <p>{user.name}</p>
-          <img className={styles.user_img} src={user.img} alt={user.name} />
-          <p>Age: {user.age}</p>
-          <p>Email: {user.email}</p>
-          <button
-            className={styles.fav_btn}
-            onClick={() => dispatch(setFavourites(user))}
-          >
-            Add to favourites
-          </button>
-        </article>
-      ))}
-    </div>
+    <>
+      {loader ? (
+        <Loader />
+      ) : (
+        <div className={styles.users_container}>
+          {usersList.map((user) => (
+            <article key={user.id} className={styles.user_card}>
+              <p>{user.name}</p>
+              <img className={styles.user_img} src={user.img} alt={user.name} />
+              <p>Age: {user.age}</p>
+              <p>Email: {user.email}</p>
+              <button
+                className={styles.fav_btn}
+                onClick={() => dispatch(setFavourites(user))}
+              >
+                Add to favourites
+              </button>
+            </article>
+          ))}
+        </div>
+      )}
+    </>
   );
 };
